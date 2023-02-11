ecce8b709db6f6a9bce6f7acb4b0cdc666dd5da8
dydtka369 <71984471+dydtka369@users.noreply.github.com>
2023-02-12 01:28:13 +0900

    js 추가

---
+++ js/witch.js
@@ -0,0 +1,41 @@
+$(function(){
+    $('.bt1').click(function(){
+      let sw = false;
+      if(sw){
+        $('#bottom_list1').css('display','none');
+        sw=!sw;
+      }else {
+        $('.bt3').css('border-bottom','0');
+        $('.bt2').css('border-bottom','0');
+        $('.bt1').css('border-bottom','3px solid #1351f9');
+        $('#bottom_list1').css('display','block');
+        $('#bottom_list2').css('display','none');
+      }
+    });
+      $('.bt2').click(function(){
+      let sw = false;
+      if(sw){
+        $('#bottom_list2').css('display','none');
+      }else {
+        $('.bt1').css('border-bottom','0');
+        $('.bt3').css('border-bottom','0');
+        $('.bt2').css('border-bottom','3px solid #1351f9');
+        $('#bottom_list1').css('display','none');
+         $('#bottom_list3').css('display','none');
+        $('#bottom_list2').css('display','block'); 
+      }
+    });
+    $('.bt3').click(function(){
+      let sw = false;
+      if(sw){
+        $('#bottom_list3').css('display','none');
+      }else {
+        $('.bt1').css('border-bottom','0');
+        $('.bt2').css('border-bottom','0');
+        $('.bt3').css('border-bottom','3px solid #1351f9');
+        $('#bottom_list1').css('display','none');
+        $('#bottom_list3').css('display','block'); 
+        $('#bottom_list2').css('display','none');
+      }
+    });
+  })
