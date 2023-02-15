c8d2d94e3b27ca96f29dde483662f4b7bc5f8a12
dydtka369 <https://github.com/dydtka369/coffeeMall.git>
2023-02-15 15:09:20 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -14,30 +14,37 @@ $(function(){
         })
     })
     let sw=false;
-    let index=0;
+    let index=1;
     let auto;
-    let mindex;
     moveSilder(index);
     autoSlider();
+    $('.slider_panel').append($('.slider_image').first().clone());
+    $('.slider_panel').prepend($('.slider_image').eq(-2).clone());
+    $('.control_button').click(function(){
+        index = $(this).index();
+        moveSilder(index+1);
+    });
     // 좌측 버튼 클릭 이벤트
     $('.left_control').click(function(){
-         index--;
-         if(index < 0) {
-             index = 5;
-         }
-         moveSilder(index);
-        // index--;
-        // moveSilder(index);
-    });
+        if(index > 1) {
+            index--;
+            moveSilder(index);
+        }else {
+            $('.slider_panel').css('left', -8680); 
+            index = 6;
+            moveSilder(index);
+        }
+    })
     // 우측버튼 클릭 이벤트
-    $('.right_control').click(function(){
-         index++;
-         if(index >= 5){
-             index = 0;
-         }
-         moveSilder(index);
-        // index++;
-        // moveSilder(index);
+    $('.right_control').stop(true).click(function(){
+        if(index < 6) {
+            index++;
+            moveSilder(index);
+        }else {
+            $('.slider_panel').css('left', 0);
+            index = 1;
+            moveSilder(index);
+        }
     });
       // 애니메이션 캔버스 영역
       $('.animation_canvas').hover(function(){
@@ -48,49 +55,23 @@ $(function(){
 
     //이미지 슬라이더 구현 함수
     function moveSilder(index) {
-        // if(index >= 0 && index < 4){
-        //     $('.left_control').fadeIn(500)
-        //     $('.right_control').fadeIn(500)
-        // }
-        // if(index == 0) {
-        //     $('.left_control').fadeOut(500)
-        //     $('.right_control').fadeIn(500)
-        // }
-        // if(index == 4) {
-        //     $('.left_control').fadeIn(500)
-        //     $('.right_control').fadeOut(500)
-        // }
         $('.slider_panel').animate({
             left : -(index*1240)
         },'slow')
-        $('.control_button').removeClass('active');
-        $('.control_button').eq(index).addClass('active');
-        // $('.slider_text').css('left',-400);
-        // $('.slider_text').eq(index).animate({
-            //     left : 0
-            // },'fast')
-            $('.slider_text').hide()
-            $('.slider_text').eq(index).fadeIn('slow'); 
     }
      //자동이미지 슬라이더 구현 함수
             function autoSlider() {
             auto = setInterval(function(){
-                if(index < 5 && sw == false){
                     $('.right_control').trigger('click');
-                }else {
-                    sw=true;
-                }
-                 if(index > 0 && sw==true){
-                    $('.left_control').trigger('click');
-                }else {
-                    sw= false;
-                }
             },2000);
         }
 
-    
-    });
+        $('.le').click(function(){
+            $('.slid_wapper').css('left',500)
+        })
 
+       
+        })
 
 
 
